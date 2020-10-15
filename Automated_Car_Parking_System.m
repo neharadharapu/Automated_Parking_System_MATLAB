7abc6cffc591e202036ee802f9c2a97707b7aa8b
neharadharapu <radharapuneha@gmail.com>
2020-10-15 18:19:33 +0530

    need to debug and rewrite
    Error using Automated_Car_Parking_System>HelperBehavioralPlanner
    Too many output arguments.
    
    Error in Automated_Car_Parking_System (line 23)
    behavioralPlanner = HelperBehavioralPlanner(routePlan, maxSteeringAngle);

--- Automated_Car_Parking_System.m
+++ Automated_Car_Parking_System.m
@@ -8,5 +8,34 @@ res=0.5;
 costmap=vehicleCostmap(combinedMap, 'CellSize', res);
 plot(costmap, 'Inflation', 'off')
 legend off
-end
 
+costmap.MapExtent % [x, width, y, height] in meters
+costmap.CellSize  % cell size in meters
+
+scenario = drivingScenario('SampleTime',0.1','StopTime',60);
+vc = vehicle(scenario);
+vehicleDims      = vehicleDimensions;
+maxSteeringAngle = 35; % in degrees
+costmap.CollisionChecker.VehicleDimensions = vehicleDims;
+currentPose = [4, 12, 0]; % [x, y, theta]
+data = load('routePlan.mat');
+routePlan = data.routePlan;
+behavioralPlanner = HelperBehavioralPlanner(routePlan, maxSteeringAngle);
+end
+function HelperBehavioralPlanner(routePlan, maxSteeringAngle)
+
+% Plot vehicle at current pose
+hold on
+helperPlotVehicle(currentPose, vehicleDims, 'DisplayName', 'Current Pose')
+legend off
+
+for n = 1 : height(routePlan)
+    % Extract the goal waypoint
+    vehiclePose = routePlan{n, 'EndPose'};
+
+    % Plot the pose
+    legendEntry = sprintf('Goal %i', n);
+    helperPlotVehicle(vehiclePose, vehicleDims, 'DisplayName', legendEntry);
+end
+hold off
+end
